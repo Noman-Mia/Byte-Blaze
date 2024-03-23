74e6d9279cfa572b187055fc9c56211bcaaa7762
Noman Mia <nomanislamnishad@gmail.com>
2024-03-23 11:59:59 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Navbar from './components/Nav'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
